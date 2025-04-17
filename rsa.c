78cbdd2a8e97e45a3c6c36e2a249bd39bee7910d
FelipeUemura11 <felipe.uemura11@gmail.com>
2025-04-17 15:40:07 -0300

    inital create

---
+++ rsa.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void){
+
+    int p, q, n, phi, e, d;
+
+    printf("Informe um numero primo (p): ");
+    scanf("%i", &p);
+
+    printf("Informe um numero primeo (q): ");
+    scanf("%i", &q);
+
+    // Calcula n e phi(totiente de n)
+    n = p * q;
+    phi = (p -1) * (q -1);
+
+    
+
+}
